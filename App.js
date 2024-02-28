46a3b2d44281ef90d78191c56809f2aa6f262d5f
afiifhirmada <158033742+afiifhirmada@users.noreply.github.com>
2024-02-28 09:55:13 +0700

    Update App.js

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ const tombol = document.querySelector(".container-tombol");
 const layar = document.querySelector("#layar");
 
 //jika tombol angka di klik oleh user
-tombol.addEventListener("click" function(e)){
+tombol.addEventListener("click", function(e)){
  let tomboldiClick = e.target;
  let nilaiTombol = tomboldiClick.innerText;
 
 //jika tombol C di klik oleh user
-if(nilaiTombol == "C"){
+if (nilaiTombol == "C") {
   layar.value = "";
-}else if(nilaiTombol == "DEL"){
+} else if(nilaiTombol == "DEL") {
   layar.value = layar.value.slice(0, -1);
-}else if(nilaiTombol == "="){
+} else if(nilaiTombol == "=") {
   layar.value = eval(layar.value);
 }else{
   layar.value = layar.value + nilaiTombol;
